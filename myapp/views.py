1fd60c4d87d1b3c49cfe88cfaa26c436fd8bdd78
hranto <hranto@Hrants-MacBook-Pro-2.local>
2023-10-01 19:54:48 +0400

    add db component

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,14 @@
+from django.forms import model_to_dict
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
     return HttpResponse('Hello World')
+
+def items(request):
+    items = TodoItem.objects.all()
+    # items = {}
+    
+    return JsonResponse({'items': [model_to_dict(item)for item in items]})
